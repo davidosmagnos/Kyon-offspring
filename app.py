8190113f7227f342c9c53d9d0b8bffe1fc49b9d4
davidosmagnos <davemagno99@gmail.com>
2021-11-02 17:34:43 +0800

    with results

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
 import os
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for,redirect
 from dog_try import faceSwap
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './'
 
 
-@app.route('/', methods=["GET", "POST"])
-def index():
-    if request.method == "GET":
-        return render_template("index.php", result={})
-    else:
-        image1 = request.files["image1"]
+@app.route('/results', methods=["GET", "POST"])
+def results():
+    if request.method == "POST":
+        image1 = request.files['image1']
+        image2 = request.files['image2']
         path1 = os.path.join(app.config['UPLOAD_FOLDER'], image1.filename)
-        image1.save(path1)
-
-        image2 = request.files["image2"]
         path2 = os.path.join(app.config['UPLOAD_FOLDER'], image2.filename)
+
+        image1.save(path1)
         image2.save(path2)
-
-
+        
         result_from_landmarks = faceSwap(path1, path2)
         os.remove(path1)
         os.remove(path2)
+        return render_template("results.php", result=result_from_landmarks)
+    else:
+        return render_template("results.php", result={})
+    
 
-        return render_template("index.php", result=result_from_landmarks)
+
+@app.route('/')
+def index():
+    if request.method == "GET":
+        return render_template("index.php")
 
 
 if __name__ == '__main__':
--- templates/results.php
+++ templates/results.php
@@ -4,7 +4,117 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KYON: Offspring Generator</title>
-    <link rel="stylesheet" href="../static/style.css">
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600;900&display=swap');
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        background-color:#0f0f0f;
+        overflow: hidden;
+    }
+    .topRect{
+        width: 16em;
+        height: 2.3em;
+        background-color: #CDD1DE;
+        margin-left: auto;
+        margin-right: -9px;
+        margin-top: -15px;
+        border-radius: 25px;
+    }
+    .appName{
+        color:#eda61c;
+        font-family: 'Montserrat-Black', sans-serif;
+        
+        margin-left: 1em;
+    }
+    .appName h1{
+        font-family: 'Montserrat';
+        margin: 3px;
+    }
+
+    .appName h1,.appName p,.appName h4{
+        margin: 3px;
+    }
+
+    .appName h4{
+        color: white !important;
+        text-align: center;
+    }
+
+    .formButtons{
+        background-color: #f4d482;
+        width: 95%;
+        height:10em;
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
+        margin-top: 1em;
+    }
+    .formButtons img{
+        width: 100px;
+        height: 100px;
+        background-color: #CDD1DE;
+        border:2px solid #4da1a9;
+        border-radius: 20px;
+        margin-top: 30px;
+        margin-left: 2em;
+    }
+
+
+    .formButtons h1{
+        display: inline;
+        position: relative;
+        bottom:1.5em;
+        left:.6em;
+    }
+
+    .generate{
+        margin-top: 3em;
+        width:fit-content;
+        margin-left: auto;
+        margin-right: auto;
+        
+    }
+
+    .generate button{
+        width: 15em;
+        height: 15em;
+        background-color: #4da1a9;
+        border-radius: 100%;
+        border-style: none;
+        color: white;
+        font-weight: bold;
+    }
+
+    .generate:hover button{
+        width: 15em;
+        height: 15em;
+        background-color: #448d94;
+        border-radius: 100%;
+        border-style: none;
+        color: white;
+        font-weight: bold;
+    }
+
+    .generate .result{
+        width: 15em;
+        height: 15em;
+        background-color: transparent;
+        border-radius: 100%;
+        border-style: none;
+    }
+    .circle{
+        background-color: #CDD1DE;
+        width: 30em;
+        height: 30em;
+        border-radius: 100%;
+        margin-left: -3.7em;
+        margin-top: -6em;
+        align-items: center;
+    }
+</style>
 </head>
 <!-- <body>
     <form method="post" enctype="multipart/form-data">
@@ -24,18 +134,24 @@
     </div> -->
 
     <div class="topRect">
-        
-    </div>
     <div class="appName">
         <h1>Kyon</h1>
         <p>Scan, Classify, and Generate</p><br>
         <h4>Offspring Generator</h4>
     </div>
-    <form method="post" enctype="multipart/form-data" action="http://192.168.1.4:5000/results">
+        
+    </div>
+    
+    <form form method="get" enctype="multipart/form-data">
         <div class="formButtons">
             <label for="image1">
                 <input type="file" name="image1" id="image1" hidden onchange="loadFile('img1')">
-               <img src="../static/placeholder_img.png" alt="" id="img1">
+                {% if result and result.error %}
+                    <p>{{ result.error }}</p>
+                {% elif result and result.image_with_landmarks %}
+                    <p><img src="{{ result.image_with_landmarks }}" alt="face with landmarks" class="result"></p>
+                {% endif %}
+            </button>
             </label>
             <h1 style="color:white;">+</h1>
             <label for="image2">
@@ -44,14 +160,13 @@
             </label>
         </div>
         <div class="generate">
-            <button>
-                <p>GENERATE<br>OFFSPRING</p>
+           
                 {% if result and result.error %}
                     <p>{{ result.error }}</p>
                 {% elif result and result.image_with_landmarks %}
                     <p><img src="{{ result.image_with_landmarks }}" alt="face with landmarks" class="result"></p>
                 {% endif %}
-            </button>
+            
         </div>
     </form>
 
