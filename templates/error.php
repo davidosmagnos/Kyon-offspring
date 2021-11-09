d252dcbe7477460fde3c97d1dc4ed4411146cb96
KeithSalandanan <keithsalandanan@gmail.com>
2021-11-09 22:44:41 +0800

    "Implemented UI for Error page"

--- templates/error.php
+++ templates/error.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/style-result.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>NAG ERROR</h1>
+<div class="body-bg-yellow">
+    <div class="topRect">
+        <div class="appName">
+            <h1 id="txt-ky" class="title">Kyon</h1>
+            <p id="txt-ky" class="desc">Scan, Classify, and Generate</p><br>
+            
+        </div>
+    </div>
+    
+
+    
+        <div id="errOutput">
+            <h1 id="txt-ky-err-msg">We cannot generate the offspring, heres a pupper instead :( </h1>
+        </div>
+        <div class="err-show">
+                <img src="../static/corgi_transparent.png" class="err-img">
+        </div>
+
+    
+        
+        <div class="circle"></div>
+   
+</div>
 </body>
 </html>
